c99ac2359a221dd513382b9da5d4f1a2dc5e0332
Thariq <thariqsingh8231@gmail.com>
2024-09-07 11:48:41 +0200

    Connection

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc:///?odbc_connect=' + urllib.parse.quote_plus(
-        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-MS27SES\\SQLEXPRESS;DATABASE=Spotify;Trusted_Connection=yes;')
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=MSCS\SQLEXPRESS;DATABASE=Spotify;Trusted_Connection=yes;')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ECHO'] = True
 
